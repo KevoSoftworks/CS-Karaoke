1c48fc85783fedf090c8192665e1afeb0d87092b
Nick Wemekamp <nickwemekamp@live.nl>
2018-10-30 11:13:40 +0100

    serverstart is a thread now

--- rpi/rpi-fpga-interface/src/com/kevosoftworks/rpifpgaiface/ServerStart.java
+++ rpi/rpi-fpga-interface/src/com/kevosoftworks/rpifpgaiface/ServerStart.java
@@ -4,8 +4,17 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.websocket.server.WebSocketHandler;
 import org.eclipse.jetty.websocket.servlet.WebSocketServletFactory;
 
-public class ServerTest {
+public class ServerStart extends Thread {
 	public static void main(String[] args) {
+		new ServerStart().start();
+	}
+	
+	@Override
+	public void run() {
+		go();
+	}
+	
+	public void go() {
 		Server server = new org.eclipse.jetty.server.Server(8080);
         WebSocketHandler wsHandler = new WebSocketHandler() {
             @Override
@@ -13,6 +22,7 @@ public class ServerTest {
                 factory.register(MyWebSocketHandler.class);
             }
         };
+        
         server.setHandler(wsHandler);
         try {
 			server.start();
